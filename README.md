01f42b40478c81e13f8b74981a09f80fc7fd0408
Assia <assiakhaldi85@gmail.com>
2025-05-06 00:27:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# basic flask template (v1.3)
+
+A barebone Flask app using Flask, Bootstrap (v5.3.0) and jQuery (v3.6.3).
+
+## Now up-to-date with Bootstrap 5.3.0 and jQuery 3.6.3
+
+## Running Locally
+
+Make sure you have Python installed
+
+```sh
+$ py -m venv .venv 
+$ .\.venv\Scripts\activate.bat
+$ pip install -r requirements.txt
+$ flask run
+```
+
+Your app should now be running on [localhost:5000](http://localhost:5000/).
---
+++ run.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Peter Simeth's basic flask pretty youtube downloader (v1.3)
+https://github.com/petersimeth/basic-flask-template
+© MIT licensed, 2018-2023
+"""
+import os
+from flask_minify  import Minify
+from flask import Flask, render_template
+
+from config import config_dict
+from apps import create_app
+
+# DEVELOPMENT_ENV = True
+
+# WARNING: Don't run with debug turned on in production!
+DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+# The configuration
+get_config_mode = 'Debug' if DEBUG else 'Production'
+
+try:
+
+    # Load the configuration using the default values
+    app_config = config_dict[get_config_mode.capitalize()]
+
+except KeyError:
+    exit('Error: Invalid <config_mode>. Expected values [Debug, Production] ')
+
+# app = Flask(__name__)
+
+# app_data = {
+#     "name": "Peter's Starter Template for a Flask Web App",
+#     "description": "A basic Flask app using bootstrap for layout",
+#     "author": "Peter Simeth",
+#     "html_title": "Peter's Starter Template for a Flask Web App",
+#     "project_name": "Starter Template",
+#     "keywords": "flask, webapp, template, basic",
+# }
+
+
+# @app.route("/")
+# def index():
+#     return render_template("index.html", app_data=app_data)
+
+
+# @app.route("/about")
+# def about():
+#     return render_template("about.html", app_data=app_data)
+
+
+# @app.route("/service")
+# def service():
+#     return render_template("service.html", app_data=app_data)
+
+
+# @app.route("/contact")
+# def contact():
+#     return render_template("contact.html", app_data=app_data)
+
+app = create_app(app_config)
+
+if not DEBUG:
+    Minify(app=app, html=True, js=False, cssless=False)
+    
+if DEBUG:
+    app.logger.info('DEBUG            = ' + str(DEBUG)             )
+    app.logger.info('Page Compression = ' + 'FALSE' if DEBUG else 'TRUE' )
+    # app.logger.info('DBMS             = ' + app_config.SQLALCHEMY_DATABASE_URI)
+
+
+# if __name__ == "__main__":
+#     app.run(debug=DEVELOPMENT_ENV)
+if __name__ == "__main__":
+    app.run()
